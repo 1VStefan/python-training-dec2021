5463515a38790395ad9786356d63f5f68d264d3b
Iulia Chiriac <iulia.chiriac@yahoo.com>
2021-12-06 12:29:08 +0200

    Add exercises

---
+++ examples/script.py
@@ -0,0 +1,33 @@
+"""
+This is a docstring for module `script` - containing some introductory notions
+about Python
+"""
+
+print('hello world!')  # inline comment
+
+# comment
+print('A very')
+print('good idea')
+
+my_shopping_list = [
+    'apples',
+    'bananas',
+    'oranges',
+    'pears',
+    'peaches',
+    'cherries'
+]
+print(my_shopping_list)
+
+x = (
+        200 + 300 + 400 + 500 + 600 + 700 + 800 + 900 +
+        1000 + 1100 + 1200 + 1300 + 1400
+)
+
+hour = 19
+if hour < 18:
+    greeting = "Good day"
+    print('hour is less than 18')
+else:
+    greeting = "Good evening"
+print(greeting)
---
+++ exercises/day1.md
@@ -0,0 +1,10 @@
+### Numeric types exercises
+
+1. Given an integer number, print its last digit. 
+1. Given a three-digit number. Find the sum of its digits.
+1. Given the integer `N` - the number of minutes that is passed since midnight -
+ how many hours and minutes are displayed on the 24h digital clock? 
+ The program should print two numbers: the number of hours (between 0 and 23) 
+ and the number of minutes (between 0 and 59). 
+ For example, if `N = 150`, then 150 minutes have passed since midnight - 
+ i.e. now it's 2:30 am. So the program should print **2 30**.
